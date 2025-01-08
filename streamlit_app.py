0f9b4995746141ebc7b6cd543215714292752766
fishfood65 <167575622+fishfood65@users.noreply.github.com>
2025-01-08 01:02:56 +0000

    test 7

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,15 +13,5 @@ if st.session_state.stage >= 1:
     name = st.text_input('Name', on_change=set_state, args=[2])
 
 if st.session_state.stage >= 2:
-    st.write(f'Hello {name}!')
-    color = st.selectbox(
-        'Pick a Color',
-        [None, 'red', 'orange', 'green', 'blue', 'violet'],
-        on_change=set_state, args=[3]
-    )
-    if color is None:
-        set_state(2)
-
-if st.session_state.stage >= 3:
-    st.write(f':{color}[Thank you!]')
+    st.write(f"✔️ You entered: {name}")
     st.button('Start Over', on_click=set_state, args=[0])
